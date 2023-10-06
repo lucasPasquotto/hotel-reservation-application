651a706c6e9e66acc67740180ed1877a3368d10f
lucas <lucaspasquotto@outlook.com>
2023-10-06 08:48:39 -0300

    feat: Testing code class

---
+++ src/Driver.java
@@ -0,0 +1,9 @@
+import model.Customer;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer("first", "last", "email");
+        System.out.println(customer);
+    }
+}
