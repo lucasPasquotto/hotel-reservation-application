d95e1e87bc90ab64458afa7b856a24543f032481
lucas <lucaspasquotto@outlook.com>
2023-10-06 08:48:04 -0300

    feat: interface IRoom and implementing classes

---
+++ src/model/FreeRoom.java
@@ -0,0 +1,22 @@
+package model;
+
+public class FreeRoom extends Room {
+    public FreeRoom(String roomNumber, RoomType type) {
+        super(roomNumber, 0.0, type);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
---
+++ src/model/Room.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.Objects;
+
+public class Room implements IRoom {
+    private String roomNumber;
+    private Double price;
+    private RoomType type;
+
+    public Room(String roomNumber, Double price, RoomType type) {
+        this.roomNumber = roomNumber;
+        this.price = price;
+        this.type = type;
+    }
+
+    @Override
+    public String getRoomNumber() {
+        return this.roomNumber;
+    }
+
+    @Override
+    public Double getRoomPrice() {
+        return this.price;
+    }
+
+    @Override
+    public RoomType getRoomType() {
+        return this.type;
+    }
+
+    @Override
+    public boolean isFree() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Room number: " + roomNumber + " Price: $" + price + " Type: " + type;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if ((obj == null) || (obj.getClass() != this.getClass()))
+            return false;
+        Room room = (Room) obj;
+        return roomNumber.equals(room.roomNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roomNumber);
+    }
+}
