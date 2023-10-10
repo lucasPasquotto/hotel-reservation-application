7073790945a4104e443351b166214e3eea0daaee
lucas <lucaspasquotto@outlook.com>
2023-10-10 08:07:50 -0300

    feat: create getters, equals and hashcode

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Reservation {
     private Customer customer;
@@ -15,14 +16,48 @@ public class Reservation {
         this.checkOutDate = checkOutDate;
     }
 
+    public Customer getCustomer() {
+        return this.customer;
+    }
+
+    public IRoom getRoom() {
+        return this.room;
+    }
+
+    public Date getCheckInDate() {
+        return this.checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return this.checkOutDate;
+    }
+
     @Override
     public String toString() {
         return "Reservation: \n"
                 + customer.getFirstName() + " " + customer.getLastName() + "\n"
-                + "Room: " + room.getRoomNumber() + " - " + room.getRoomType()
-                + "Price: " + room.getRoomPrice() + " price per night"
-                + "Checkin: " + checkInDate
+                + "Room: " + room.getRoomNumber() + " - " + room.getRoomType() + "\n"
+                + "Price: " + room.getRoomPrice() + " price per night" + "\n"
+                + "Checkin: " + checkInDate + "\n"
                 + "Checkout: " + checkOutDate;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if ((obj == null) || (obj.getClass() != this.getClass()))
+            return false;
+        Reservation reservation = (Reservation) obj;
+        return customer.equals(reservation.customer)
+                && room.equals(reservation.room)
+                && checkInDate.equals(reservation.checkInDate)
+                && checkOutDate.equals(reservation.checkOutDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customer, room, checkInDate, checkOutDate);
     }
 
 }
