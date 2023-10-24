4d1b6bbf92e8493a312b69600c1b4084dd6670dd
lucas <lucaspasquotto@outlook.com>
2023-10-24 07:58:28 -0300

    feat: methods implementation on admin menu

--- src/view/AdminMenu.java
+++ src/view/AdminMenu.java
@@ -1,14 +1,26 @@
 package view;
 
+import api.AdminResource;
+import model.Customer;
+import model.IRoom;
+import model.Room;
+import model.RoomType;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Scanner;
 
 public class AdminMenu {
+
+    public static AdminResource adminResource = AdminResource.getInstance();
 
     public static void adminMenu() {
         Scanner scanner = new Scanner(System.in);
         String input;
 
-        System.out.println("Welcome to the Hotel Reservation Application!");
+        System.out.println("\nAdmin menu");
         System.out.println("-------------------------------------------");
         System.out.println("1. See all Customers");
         System.out.println("2. See all Rooms");
@@ -23,20 +35,24 @@ public class AdminMenu {
 
             switch (input.charAt(0)) {
                 case '1':
-                    System.out.println("Item 1.");
+                    seeAllCustomers();
+                    adminMenu();
                     break;
                 case '2':
-                    System.out.println("Item 2.");
+                    seeAllRooms();
+                    adminMenu();
                     break;
                 case '3':
-                    System.out.println("Item 3.");
+                    System.out.println("\nHere is all reservations.");
+                    adminResource.displayAllReservations();
+                    adminMenu();
                     break;
                 case '4':
-                    System.out.println("Item 4.");
+                    addARoom();
+                    adminMenu();
                     break;
                 case '5':
-                    System.out.println("Option 5 Admin menu.");
-                    MainMenu.mainMenu();
+                    System.out.println("Back to main menu!");
                     break;
                 default:
                     System.out.println("Please select an valid option.");
@@ -46,6 +62,71 @@ public class AdminMenu {
         } catch (StringIndexOutOfBoundsException ex) {
             System.out.println("Please select an valid option.");
             adminMenu();
+        }
+    }
+
+    private static void addARoom() {
+        List<IRoom> rooms = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        String addAnotherRoom;
+        do {
+            System.out.println("\nEnter room number: ");
+            String roomNumber = scanner.nextLine();
+
+            System.out.println("Enter price per night: ");
+            String price = scanner.nextLine();
+
+            Double roomPrice = 0.0;
+            try {
+                roomPrice = Double.parseDouble(price);
+            } catch (NumberFormatException ex) {
+                System.out.println("Invalid input for price");
+                addARoom();
+            }
+
+            System.out.println("Enter room type (1 for a single bed, 2 for a double bed): ");
+            String type = scanner.nextLine();
+
+            RoomType roomType = null;
+            if ("1".equals(type)) {
+                roomType = RoomType.SINGLE;
+            } else if ("2".equals(type)) {
+                roomType = RoomType.DOUBLE;
+            } else {
+                System.out.println("Invalid input, try to select 1 or 2");
+                addARoom();
+            }
+
+            Room room = new Room(roomNumber, roomPrice, roomType);
+            rooms.add(room);
+
+            System.out.println("\nWould you like to add another room? [Y/N]");
+            addAnotherRoom = scanner.nextLine();
+        } while ("y".equals(addAnotherRoom.toLowerCase().trim()));
+
+        adminResource.addRoom(rooms);
+    }
+
+    private static void seeAllRooms() {
+        Collection<IRoom> rooms = adminResource.getAllRooms();
+        if (rooms.isEmpty()) {
+            System.out.println("No rooms founded.");
+        } else {
+            for (IRoom room : rooms) {
+                System.out.println(room.toString());
+            }
+        }
+    }
+
+    private static void seeAllCustomers() {
+        Collection<Customer> customers = adminResource.getAllCustomers();
+        if (customers.isEmpty()) {
+            System.out.println("No customers founded");
+        } else {
+            for (Customer customer : customers) {
+                System.out.println(customer.toString());
+            }
         }
     }
 }
